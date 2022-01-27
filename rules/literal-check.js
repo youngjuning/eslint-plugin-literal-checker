8cee4c4e41c561d236dbe44ba365f9eb532015c9
Carlos Ortiz <charlie@cerby.com>
2022-01-27 01:58:59 -0600

    feat: add TemplateString support (#2)
    * chore: add .gitignore
    * feat: add support for template strings

--- rules/literal-check.js
+++ rules/literal-check.js
@@ -1,24 +1,39 @@
 'use strict';
 
+/**
+ * 
+ * @param {string} value Literal to check against the `denyList`
+ * @param  context Plugin Context
+ * @param {Node} node Current AST Node
+ * @param {string[]} denyList List of prohibited literals
+ */
+function checkValue(value, context, node, allowList, denyList) {
+  denyList.forEach(denyItem => {
+    if(value.indexOf(denyItem) !== -1 && allowList.indexOf(value) === -1) {
+      let message = `You should not use "${denyItem}".`;
+      context.report({node: node, message: message});
+    }
+  });
+}
+
+
 module.exports = function(context) {
-  let blacklist = [];
-  let whitelist = [];
+  let denyList = [];
+  let allowList = [];
   if (Array.isArray(context.options[0])) {
-    blacklist = context.options[0];
+    denyList = context.options[0];
   }
   if (Array.isArray(context.options[1])) {
-    whitelist = context.options[1];
+    allowList = context.options[1];
   }
   return {
     'Literal': node => {
-      let message = null;
       let value = String(node.value);
-      blacklist.forEach(blackItem => {
-        if(value.indexOf(blackItem) !== -1 && whitelist.indexOf(value) === -1) {
-          message = `You should not use "${blackItem}".`;
-          context.report({node: node, message: message});
-        }
-      });
+      checkValue(value, context, node, allowList,  denyList)
+    },
+    'TemplateElement': node => {
+      let value = String(node.value.raw);
+      checkValue(value, context, node, allowList, denyList)
     }
   };
 };
